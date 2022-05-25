38e4648692094af9aef987431961ab6b9251a90a
NataR 78 <vip.aleksa78@mail.ru>
2022-05-25 21:19:58 +0300

    calculator

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-let clear = document.getElementById("clear")
-operator_btn.addEventListener("click", function (event){
+let display = document.getElementById("display")
+let calc = document.querySelector(".calc")
+let button = document.querySelector("button")
+
+calc.addEventListener("click", function (event) {
+    let value = event.target.innerText
+
+    let space = " "
+
+    console.log(event.target.closest("button"))
+
+    if (event.target.closest("button") != null) {
+
+        if (value === "Clear") {
+            display.value = ""
+        }
+        else if (value === "=") {
+            display.value += "=" + eval(display.value)
+        }
+        else if (value === "X") {
+            display.value += "*"
+
+            // alert("X")
+            // display.value = eval(display.innerText)
+        }
+
+        else if (value === "%") {
+            display.value += "%" + space + "=" + space + eval(display.value)
+        }
+
+        else if (value === "Delete") {
+            display.value += "Delete"-1  eval(display.value)
+            
+        }
+
+        else {
+            display.value += value
+
+        }
+    }
+
 
 })
+
+
